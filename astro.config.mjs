a7df6000b258949b13c184d27b6fd6d8729553bb
Xuling Chang <xuling.chang@live.com>
2025-12-31 10:09:39 +0800

    增加公式支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bigbookplus.github.io',
   integrations: [mdx(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
   vite: {
     build: {
       rollupOptions: {
